27692f377ad529b3f695a87821f5630e35684677
aoi1049 <aoi1049@gmail.com>
2019-07-15 01:24:05 +0900

    fix init.el for typescript.

--- init.el
+++ init.el
@@ -276,7 +276,7 @@
  '(anzu-search-threshold 1000)
  '(package-selected-packages
    (quote
-    (lsp-ui lsp-go use-package lsp-mode go-eldoc markdown-mode exec-path-from-shell go-complete company-go go-mode flycheck web-mode vue-mode tuareg scss-mode ruby-refactor ruby-electric ruby-block rainbow-delimiters python-mode py-autopep8 php-mode php-completion paredit ocp-indent jedi ipython helm-gtags haml-mode flymake-python-pyflakes elpy coffee-fof caml cake2 cake auto-indent-mode anzu ac-nrepl)))
+    (company tide typescript-mode lsp-ui lsp-go use-package lsp-mode go-eldoc markdown-mode exec-path-from-shell go-complete company-go go-mode flycheck web-mode vue-mode tuareg scss-mode ruby-refactor ruby-electric ruby-block rainbow-delimiters python-mode py-autopep8 php-mode php-completion paredit ocp-indent jedi ipython helm-gtags haml-mode flymake-python-pyflakes elpy coffee-fof caml cake2 cake auto-indent-mode anzu ac-nrepl)))
  '(safe-local-variable-values (quote ((enconding . utf-8)))))
 
 ;;; --- auto complete
@@ -410,6 +410,57 @@
 	(setq indent-tab-mode nil)
 )
 (add-hook 'vue-mode-hook 'my-vue-mode-hook)
+
+;;; --- typescript mode
+;; need to install typescript-mode.
+(require 'typescript-mode)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+;; need to install company and tide.
+(require 'tide)
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (tide-setup)
+            (flycheck-mode t)
+            (setq flycheck-check-syntax-automatically '(save mode-enabled))
+            (eldoc-mode t)
+            (company-mode-on)))
+;; customize company mode for typescript.
+(require 'company)
+;; to choice C-n, C-p.
+(define-key company-active-map (kbd "M-n") nil)
+(define-key company-active-map (kbd "M-p") nil)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+;; reset C-h from default mapping.
+(define-key company-active-map (kbd "C-h") nil)
+;; use tab for select item from map.
+(define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+;; show document.
+(define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
+
+(setq company-minimum-prefix-length 1)
+(setq company-selection-wrap-around t)
+
+;; color setting.
+(set-face-attribute 'company-tooltip nil
+                    :foreground "black"
+                    :background "lightgray")
+(set-face-attribute 'company-preview-common nil
+                    :foreground "dark gray"
+                    :background "black"
+                    :underline t)
+(set-face-attribute 'company-tooltip-selection nil
+                    :background "steelblue"
+                    :foreground "white")
+(set-face-attribute 'company-tooltip-common nil
+                    :foreground "black"
+                    :underline t)
+(set-face-attribute 'company-tooltip-common-selection nil
+                    :foreground "white"
+                    :background "steelblue"
+                    :underline t)
+(set-face-attribute 'company-tooltip-annotation nil
+                    :foreground "red")
 
 ;;; --- Web mode (php, pl, js, html)
 (require 'web-mode)
