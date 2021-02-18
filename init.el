b49be2d65df4eaf6c16a9c23c228828c25afc109
Asya-kawai <kawai-toshiki@aintek.xyz>
2021-02-18 11:43:23 +0900

    :fire: Deleted gore-mode(go repl mode) because gore didn't match my usecase.

--- init.el
+++ init.el
@@ -231,7 +231,6 @@
                       ;;; --- go mode ---
                       go-mode
                       go-dlv
-                      gorepl-mode
                       ;;; --- vue mode ---
                       vue-mode
                       mmm-mode
@@ -557,16 +556,11 @@
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 
-;; NOTE: go get gore before.
-(use-package gorepl-mode
-  :ensure t)
-
 (use-package go-mode
   :ensure t
   :init (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
   :hook ((go-mode . lsp-deferred)
-         (go-mode . company-mode)
-         (go-mode . gorepl-mode)))
+         (go-mode . company-mode)))
 
 ;;; --- python-mode
 ;;; extends python mode
